6d1064f5d694468b1f206757df299cefd5203137
littlezim <littlezim18@gmail.com>
2016-09-15 11:16:42 -0700

    Fixed Migrations

---
+++ MedAgenda/MedAgenda.API/Migrations/201609151757469_Fixed DateTime for PatientCheckIn.cs
@@ -0,0 +1,26 @@
+namespace MedAgenda.API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixedDateTimeforPatientCheckIn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PatientCheckIns", "CheckInTime", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Patients", "Medications", c => c.String());
+            AddColumn("dbo.Patients", "ChronicConditions", c => c.String());
+            AddColumn("dbo.Patients", "Allergies", c => c.String());
+            AddColumn("dbo.Patients", "Symptoms", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Patients", "Symptoms");
+            DropColumn("dbo.Patients", "Allergies");
+            DropColumn("dbo.Patients", "ChronicConditions");
+            DropColumn("dbo.Patients", "Medications");
+            DropColumn("dbo.PatientCheckIns", "CheckInTime");
+        }
+    }
+}
